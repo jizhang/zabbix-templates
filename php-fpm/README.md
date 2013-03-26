511c3e3c9048053607af1e90350b62036240b7e1
Ji Zhang <jizhang@anjuke.com>
2013-03-26 12:15:53 +0800

    add php-fpm template

---
+++ php-fpm/README.md
@@ -0,0 +1,50 @@
+Template php-fpm
+================
+
+Show php-fpm statistics in Zabbix.
+
+INSTALL
+-------
+
+Assume the Zabbix agent is installed in /zabbix-agent/ directory.
+
+### Configure php-fpm
+
+Open the php-fpm pool's configuration file, uncomment the 'pm.status=' directive:
+
+pm.status_path = /php-fpm_status
+
+Since php-fpm's statistics is collected by different pools, so you need to create corresponding hosts for them.
+
+### Configure Nginx
+
+Add the following lines to Nginx configuration:
+
+server {
+    listen 10061;
+
+    location /php-fpm_status {
+        fastcgi_pass 127.0.0.1:9000;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        include fastcgi_params;
+    }
+}
+
+After restarting both php-fpm and nginx, try the following command to test:
+
+$ curl http://127.0.0.1:10061/php-fpm_status
+
+### Add User Parameters
+
+Copy php-fpm-params.conf to /zabbix-agent/etc/zabbix_agentd.conf.d/. Restart Zabbix agent.
+
+### Import Template
+
+Import php-fpm-template.xml, and link it to a host. Set the host macro {$PHP_FPM_STATUS_URL} if needed.
+
+
+CREDITS
+-------
+
+Some of the scripts are form http://github.com/zbal/zabbix.
+
---
+++ php-fpm/php-fpm-check.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+##################################
+# Zabbix monitoring script
+#
+# php-fpm:
+#  - anything available via FPM status page
+#
+##################################
+# Contact:
+#  vincent.viallet@gmail.com
+##################################
+# ChangeLog:
+#  20100922	VV	initial creation
+##################################
+
+# Zabbix requested parameter
+ZBX_REQ_DATA="$1"
+ZBX_REQ_DATA_URL="$2"
+
+# Nginx defaults
+NGINX_STATUS_DEFAULT_URL="http://localhost:80/php-fpm_status"
+WGET_BIN="/usr/bin/wget"
+
+#
+# Error handling:
+#  - need to be displayable in Zabbix (avoid NOT_SUPPORTED)
+#  - items need to be of type "float" (allow negative + float)
+#
+ERROR_NO_ACCESS_FILE="-0.9900"
+ERROR_NO_ACCESS="-0.9901"
+ERROR_WRONG_PARAM="-0.9902"
+ERROR_DATA="-0.9903" # either can not connect /	bad host / bad port
+
+# Handle host and port if non-default
+if [ ! -z "$ZBX_REQ_DATA_URL" ]; then
+  URL="$ZBX_REQ_DATA_URL"
+else
+  URL="$NGINX_STATUS_DEFAULT_URL"
+fi
+
+# save the nginx stats in a variable for future parsing
+NGINX_STATS=$($WGET_BIN -q $URL -O - 2> /dev/null)
+
+# error during retrieve
+if [ $? -ne 0 -o -z "$NGINX_STATS" ]; then
+  echo $ERROR_DATA
+  exit 1
+fi
+
+# 
+# Extract data from nginx stats
+#
+RESULT=$(echo "$NGINX_STATS" | awk 'match($0, "^'"$ZBX_REQ_DATA"':\\s+(.*)", a) { print a[1] }')
+if [ $? -ne 0 -o -z "$RESULT" ]; then
+    echo $ERROR_WRONG_PARAM
+    exit 1
+fi
+
+echo $RESULT
+
+exit 0
