ecc33659e4c837137ccba771dcce8625edd6dae4
Ji Zhang <jizhang@anjuke.com>
2013-03-28 22:53:52 +0800

    jvm

---
+++ jvm/README.md
@@ -0,0 +1,34 @@
+Template JVM
+============
+
+Show JVM statistics in Zabbix.
+
+INSTALL
+-------
+
+Assume the Zabbix agent is installed in /zabbix-agent/ directory.
+
+### A Compatible JDK
+
+This package uses `jstat` and `jstack` commands to gather information of jVM.
+
+### Start Daemon
+
+Since the Zabbix agent runs in zabbix user, making it impossible to attache to JVMs running under other users. The solution is to start a daemon under that user and provide socket access to gather the information.
+
+$ nohup jvm-service.pl >/dev/null 2>&1 &
+
+### Install Script and Add User Parameters
+
+Copy jvm-check.pl to /zabbix-agent/bin/. Copy jvm-params.conf to /zabbix-agent/etc/zabbix_agentd.conf.d/. Restart Zabbix agent.
+
+### Import Template
+
+Import jvm-template.xml, and link it to a host. Set the host macro {$JVMPORT} to which the JVM you want to monitor bind.
+
+HOW IT WORKS
+------------
+
+To gather information more effectively, I didn't use a lot of user parameters in the configuration file, to run multiple times. Instead, I used the 'zabbix agent trapper' data type, and run another script sending multiple data items to zabbix server.
+
+Again, instead of setup a cron job for the script, I used another 'zabbix agent' data type to let the server trigger this script.
--- jvm/jvm-check.pl
+++ jvm/jvm-check.pl
@@ -9,7 +9,7 @@ use constant COLLECTOR_UP => 1;
 use constant COLLECTOR_DOWN => 0;
 
 sub stats {
-    my ($jvmport, $service_addr) = @_;
+    my ($host, $jvmport, $service_addr, $zabbix_bin) = @_;
     my ($service_ip, $service_port) = split(/:/, $service_addr);
 
     my $client = IO::Socket::INET->new(
@@ -21,9 +21,20 @@ sub stats {
     print $client "JVMPORT $jvmport\n";
     my $status = <$client>;
     chomp($status);
-    if ($status != 'OK') {
+    if ($status ne 'OK') {
         print COLLECTOR_DOWN;
         return 2;
+    }
+
+    while (<$client>) {
+        chomp;
+        my ($key, $value) = split /\s+/;
+        if (!$key) {
+            next;
+        }
+        $key = lc $key;
+        $key =~ s/%/p/g;
+        `$zabbix_bin -s "$host" -k "jvm.$key" -o "$value"`
     }
 
     close($client);
@@ -33,17 +44,23 @@ sub stats {
 }
 
 my $help = 0;
+my $host = '';
 my $jvmport = 0;
 my $service_addr = '127.0.0.1:10060';
+my $zabbix_sender = '/usr/local/zabbix-agent-ops/bin/zabbix_sender';
+my $zabbix_conf = '/usr/local/zabbix-agent-ops/etc/zabbix_agentd.conf';
 
 GetOptions (
     'help|?' => \$help,
+    'host=s' => \$host,
     'jvmport=i' => \$jvmport,
-    'service-addr=s' => \$service_addr
+    'service-addr=s' => \$service_addr,
+    'zabbix-sender=s' => \$zabbix_sender,
+    'zabbix-conf=s' => \$zabbix_conf
 ) || pod2usage(2);
-pod2usage(1) if $help || !$jvmport;
+pod2usage(1) if $help || !$jvmport || !$host;
 
-exit(stats($jvmport, $service_addr));
+exit(stats($host, $jvmport, $service_addr, "$zabbix_sender -c \"$zabbix_conf\""));
 
 __END__
 
@@ -56,8 +73,11 @@ JVM Statistics
 jvm-check.pl [options]
 
  Options:
-   -help         brief help message
-   -jvmport      JVM port
-   -service-addr service address "ip:port"
+   -help          brief help message
+   -host          hostname recognized by zabbix server
+   -jvmport       JVM port
+   -service-addr  service address "ip:port"
+   -zabbix-sender path to zabbix_sender binary
+   -zabbix-conf   path to zabbix_agentd.conf
 
 =cut
